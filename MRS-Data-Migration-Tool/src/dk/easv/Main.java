29a6391fadba66ca77637b2f8356281eba2a3f42
jeppeml <jeppeml@users.noreply.github.com>
2021-11-24 12:35:42 +0100

    Update Main.java

--- MRS-Data-Migration-Tool/src/dk/easv/Main.java
+++ MRS-Data-Migration-Tool/src/dk/easv/Main.java
@@ -22,25 +22,92 @@ public class Main {
         ds.setPortNumber(1433);
         ds.setServerName("10.176.111.31");
 
-        List<String> movies = Files.readAllLines(Path.of("data/movie_titles.txt"));
-        System.out.println("Movies: " + movies.size());
-        for(String s : movies){
-            String[] values = s.split(",");
-            int id = Integer.parseInt(values[0]);
-            String title = values[2];
-            int year = Integer.parseInt(values[1]);
-            String sql = "INSERT INTO Movies (Id, Title, Year) VALUES ("+
-                    id + ", '" +
-                    title + "', " +
-                    year + ");";
+        //migrateMovies(ds);
+        //migrateUsers(ds);
+        long start = System.currentTimeMillis();
+        System.out.println("Starting...");
+        migrateRatings(ds);
+        System.out.println("Time taken: " + (System.currentTimeMillis()-start));
+    }
+    private static void migrateRatings(SQLServerDataSource ds){
+        List<String> movies = null;
+        try {
+            movies = Files.readAllLines(Path.of("data/ratings.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }//movie user rating
+        try (Connection con = ds.getConnection()) {
+            String sql = "INSERT INTO Users (MovieId, UserId, Rating) VALUES (?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(sql);
+            for (String s : movies) {
+                String[] values = s.split(",");
+                int movieId = Integer.parseInt(values[0]);
+                int userId = Integer.parseInt(values[1]);
+                int rating = Integer.parseInt(values[2]);
 
-            try (Connection con = ds.getConnection()) {
-                PreparedStatement st = con.prepareStatement(sql);
-                int affected = st.executeUpdate();
-                System.out.println("Affected rows: " + affected);
-            } catch (SQLException e) {
-                e.printStackTrace();
+                st.setInt(1, movieId);
+                st.setInt(2, userId);
+                st.setInt(3, rating);
+                st.addBatch();
             }
+            st.executeBatch();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void migrateUsers(SQLServerDataSource ds){
+        List<String> movies = null;
+        try {
+            movies = Files.readAllLines(Path.of("data/users.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (Connection con = ds.getConnection()) {
+            String sql = "INSERT INTO Users (Id, [Name]) VALUES (?, ?)";
+            PreparedStatement st = con.prepareStatement(sql);
+            for (String s : movies) {
+                String[] values = s.split(",");
+                int id = Integer.parseInt(values[0]);
+                String name = values[1];
+
+                st.setInt(1, id);
+                st.setString(2, name);
+                st.addBatch();
+            }
+            st.executeBatch();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void migrateMovies(SQLServerDataSource ds){
+        List<String> movies = null;
+        try {
+            movies = Files.readAllLines(Path.of("data/movie_titles.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (Connection con = ds.getConnection()) {
+            String sql = "INSERT INTO Movies (Id, Title, Year) VALUES (?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(sql);
+            for (String s : movies) {
+                String[] values = s.split(",");
+                int id = Integer.parseInt(values[0]);
+                String title = values[2];
+                int year = Integer.parseInt(values[1]);
+                //System.out.println("input: " + s);
+
+                st.setInt(1, id);
+                st.setString(2, title);
+                st.setInt(3, year);
+                st.addBatch();
+            }
+            st.executeBatch();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
